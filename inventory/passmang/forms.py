0d5fcfd3ca45e86ea06cf5c6406f08302b4b5d87
Muhammad Bilal <mr.bilal2066@gmail.com>
2021-10-22 22:41:40 +0500

    extras

--- inventory/passmang/forms.py
+++ inventory/passmang/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.forms.widgets import PasswordInput
-from .models import SecureNote
-
+from .models import SecureContacts
 
 class storepass(forms.Form):
     hashed = forms.CharField(widget=PasswordInput())
@@ -11,3 +10,9 @@ class storepass(forms.Form):
 class storenote(forms.Form):
     hashed = forms.CharField()
     account_for = forms.CharField(max_length=30)
+
+
+class contactForm(forms.ModelForm):
+    class Meta:
+        model = SecureContacts
+        exclude = ('user_id',)
--- inventory/passmang/models.py
+++ inventory/passmang/models.py
@@ -8,5 +8,14 @@ class SecureNote(models.Model):
     letter = models.CharField(max_length=1)
 
 
-# class SecureContacts(models.Model):
-#     name = models.CharField(max_length=75)
+class SecureContacts(models.Model):
+    user_id = models.IntegerField()
+    name = models.CharField(max_length=75)
+    number = models.CharField(max_length=12)
+    email = models.CharField(max_length=40, null= True, blank= True)
+    company = models.CharField(max_length=40, null= True, blank= True)
+    designation = models.CharField(max_length=30, null= True, blank= True)
+    address = models.CharField(max_length=200, null= True, blank= True)
+    website = models.CharField(max_length=80, null= True, blank= True)
+    relationship = models.CharField(max_length=30, null= True, blank= True)
+    note = models.CharField(max_length=100, null= True, blank= True)
